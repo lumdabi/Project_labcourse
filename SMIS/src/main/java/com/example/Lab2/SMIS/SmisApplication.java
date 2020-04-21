4c140e9066d9261ef309532f915634b7db075bff
Fatlum Dabinovci <fdabinovci@gmail.com>
2020-04-21 23:46:40 +0200

    fist commit

---
+++ SMIS/src/main/java/com/example/Lab2/SMIS/SmisApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab2.SMIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmisApplication.class, args);
+	}
+
+}
---
+++ SMIS/src/test/java/com/example/Lab2/SMIS/SmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab2.SMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
